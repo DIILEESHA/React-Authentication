46c866b8dcd69b04ed78a0989720bceb2cf9c42d
DILEESHA <dileeshanawarathna18@gmail.com>
2021-09-24 04:49:59 +0530

    create hooks

--- src/login.js
+++ src/login.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './login.css'
 
 function login() {
+
+    const[username,setusername]=useState('')
+    const[password,setpassword]=useState('')
+
     return <div>
 
         <div className='row justify-content-center'>
         <div className='col-md-4'>
         <h1><b><i>Authentication</i></b></h1>
         <form>
-            <input type = "text" placeholder = "Enter Name" className="form-control"/>
-            <input type = "text" placeholder = "Enter Name" className="form-control"/>
+            <input type = "text" placeholder = "username" className="form-control" value={username} onChange={(e)=>{setusername(e.target.value)}}/>
+            <input type = "text" placeholder = "password" className="form-control" value={password} onChange={(e)=>{setpassword(e.target.value)}}/>
             <input type ="submit" className='btn btn-primary'/>
             </form>
         </div>
